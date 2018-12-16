a76e15389fe6b8a2362983f096a26522f696d57f
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-16 05:27:43 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,8 @@ def reformat_languages(languages)
   languages.each do |key, value|
     value.each do |k, v|
       if output.key?(k)
-        output[k][:style][key]
+        output[k][:style] << [key]
+        output[k][:style].flatten!
       else
         output[k] = {
           :type => v[:type],
